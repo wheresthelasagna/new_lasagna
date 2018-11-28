77bd60eb6eda1dc20e45cc449425d40cefc029b0
Wheres the Lasagna <37559146+wheresthelasagna@users.noreply.github.com>
2018-11-28 01:06:58 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -12,15 +12,22 @@ screen = pg.display.set_mode(WINDOW_SIZE) #initiate the window
 
 move_right = False
 move_left = False
+move_up = False
+move_down = False
 
 while True: #game loop
 	screen.fill(BLACK)
-	screen.blit(pupper,pos)
+	screen.blit(thor,pos)
 	pg.draw.rect(screen,BLUE,(WIDTH/2,HEIGHT/2,pupper.get_width(),pupper.get_height()))
 	if move_right:
-		pos[0] += 4
+		pos[0] += 8
 	if move_left:
-		pos[0] -= 4
+		pos[0] -= 8
+	if move_up:
+		pos[1] -= 4
+	if move_down:
+		pos[1] += 4
+
 	for event in pg.event.get():
 		if event.type == QUIT:
 			pg.quit()
@@ -33,10 +40,19 @@ while True: #game loop
 				move_right = True
 			if event.key == K_LEFT:
 				move_left = True
+			if event.key == K_UP:
+				move_up = True
+			if event.key == K_DOWN:
+				move_down = True
 		if event.type == KEYUP:
 			if event.key == K_RIGHT:
 				move_right = False
 			if event.key == K_LEFT:
 				move_left = False
+			if event.key == K_UP:
+				move_up = False
+			if event.key == K_DOWN:
+				move_down = False
+
 	pg.display.flip() #update display
 	pg.time.Clock().tick(60) #maintain 60 fps
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import pygame as pg
 #settings
-WIDTH = 320 #x
-HEIGHT = 400 #y 4-5
+WIDTH = 600 #x
+HEIGHT = 480 #y 4-5
 WINDOW_SIZE = (WIDTH,HEIGHT) #(x,y)
 
 #colors R G B
@@ -13,4 +13,5 @@ BLUE = (0,0,255)
 
 pos = [0,0] #avatar position
 pupper = pg.transform.scale(pg.image.load('pupper_bread.gif'),(75,75)) #create player avatar
-#pupper_rect = pg.Rect(pos[0],pos[1],pupper.get_width(),pupper.get_height()) 
+thor = pg.transform.scale(pg.image.load('thor.png'),(181,100)) # 1342x742 1.808625
+#pupper_rect = pg.Rect(pos[0],pos[1],pupper.get_width(),pupper.get_height())
